df1feefa166c954ddd673c735c6ca4befae24288
rhyswill22 <rhwi3413@colorado.edu>
2024-07-02 16:15:32 -0600

    Update ica12.js

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -3,25 +3,60 @@ const newQuote = document.querySelector('#js-new-quote');
 
 // Write an event listener to check if the button is clicked. When the button is clicked, run a function called "getQuote".
 
-newQuote.addEventListener('click', getTrivia);
-
-const getAnswerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer)
+newQuote.addEventListener('click', getQuote);
 
 // Write the function declaration, and check the button click works by returning a message in the console everytime the button is clicked.
-
+function displayAnswer(){
+    console.log('Display answer was clicked');
+}
 
 // Add a new variable that holds the API endpoint: 
 
-// https://trivia.cyberwisp.com/getrandomchristmasquestion
+const apiEndpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
 
 // Change the getQuote function to use the fetch method to get a random quote from that endpoint.
 
-// If successful, output the quote to the console
+async function getQuote()
+{
+try{
+const response = await fetch(apiEndpoint);
+    if(!response.ok){
+        throw Error(response.statusText)
 
-// If it fails, output an error message to the console AND via alert
+    }
+
+
+    const json = await response.json();
+    // If successful, output the quote to the console
+    displayQuote(json.question);
+    }
+
+    catch(error){
+        // If it fails, output an error message to the console AND via alert
+        console.log(error);
+        alert('Error. Could not fetch quote');
+
+    }
+}
+
+
+
+
 
 // Write a second function called "displayQuote" that will display the text of a fetched quote in the HTML element with an id of js-quote-text.
 
+
+function displayQuote(quote)
+{
+    const quoteText = document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
+}
+
+
 // Adjust getQuote to run displayQuote at the proper place in the code.
 
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
+
 // Notice when you refresh that a quote isn't displayed. Fix that.
+
+getQuote();
