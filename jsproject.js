5097fb54b7dd63f05da7fcc0fe8f81f1ddd9a0d9
rhyswill22 <rhwi3413@colorado.edu>
2024-07-06 13:16:44 -0600

    JS PROJECT

--- jsproject.js
+++ jsproject.js
@@ -1,48 +1,5 @@
 
-    // const questions = [
-
-    //     { question: "What American novelist is well-known for his sparse use of punctuation and once claimed that to use quotation marks is to blot the page up with weird little marks? This author is associated with the Southern Gothic, Western, and post-apocalyptic genres."
-    //     , answer: "Cormac McCarthy" },
-
-    //     { question: "What Anthony Horowitz-created character is sometimes referred to as a Teenage James Bond? He is the main character in a series of books that starts with Stormbreaker.", 
-    //     answer: "Alex Rider" },
-
-    //     { question: "Charles and Caroline Ingalls, the parents from the “Little House on the Prairie” series of books by Laura Ingalls Wilder, are buried in which northern state, which also served as the setting for the last 5 books in the series?",
-    //     answer: "South Dakota" },
-
-    //     { question: "The collective name for owls is parliament. This noun arose because of a description of a meeting of owls in what author's works?",
-
-    //     answer: "C.S. Lewis"},
-
-    //     { question: "Considered one of the innovators of creative nonfiction via New Journalism, what American authors best known work The Executioners Song won the 1979 Pulitzer Prize for fiction? This postal author also ran in the Democratic primary for NYC's mayoral race of 1969 with a platform including the secession of New York City as the 51st US state.",
-    //     answer: "Norman Mailer"},
-
-    //     {question: "Orleanna Price and her four daughters (Rachel, Leah, Adah and Ruth May) are living in the Belgian Congo near the Kwilu River in what 1998 award-winning novel by Barbara Kingsolver?",
-
-    //     answer: "The Poisonwood Bible"},
-
-    //     {question: "Author John Green set his tearjerking 2012 bestseller, The Fault in Our Stars, in what state capital that is also Green's hometown?",
-
-    //     answer: "Indianapolis"},
-
-    //     {question: "Adapted into a 2020 Netflix film, Ma Rainey's Black Bottom is one of the ten plays about the Black American experience known as the Pittsburgh Cycle, by what playwright?",
-
-    //     answer: "August Wilson"},
-
-    //     {question: "The most famous work of what Roman poet born in 43 BC begins My intention is to tell of bodies changed to different forms?",
-
-    //     answer: "Ovid"},
-
-    //     {question: "What 13-letter German loanword means a novel that focuses on the psychological and personal growth of the protagonist from youth to adulthood?",
-
-    //     answer: "Bildungsroman"},
-
-    //     {question: "'Hell is other people' is the most famous line to come out of what Sartre play about three sinners trapped together forever?",
-
-    //     answer: "No Exit"}
-
-  // end of document
-
+ 
 // functions after the content is loaded
 
   document.addEventListener('DOMContentLoaded', () => {
@@ -54,6 +11,20 @@
       number.addEventListener('click', () => {
         const selectedNumber = number.getAttribute('data-number');
         phoneNumberInput.value += selectedNumber;
+
+
+        if(phoneNumberInput.value.length === 10)
+        {
+        const submitPhoneNumber = confirm(`Is this your phone number: ${phoneNumberInput.value}`);
+
+        if(submitPhoneNumber)
+        {
+            alert(`Submitted phone number: ${phoneNumberInput.value}`);
+        }
+        else{
+            phoneNumberInput.value = '';
+            }
+        }
       });
     });
   });
