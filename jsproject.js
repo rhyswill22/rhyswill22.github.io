69975d3b84296ba24b42d853a9c75a6e76fbbb93
rhyswill22 <rhwi3413@colorado.edu>
2024-07-06 13:52:49 -0600

    Update jsproject.js

--- jsproject.js
+++ jsproject.js
@@ -6,17 +6,40 @@
     const numbers = document.querySelectorAll('.number');
     const phoneNumberInput = document.getElementById('phoneNumber');
   
+
+    const phoneNumberFormat = (value) => {
+
+        let formattedValue = '';
+        if (formattedValue.length > 3)
+
+          {
+            formattedValue = `(${formattedValue.slice(0, 3)}) ${formattedValue.slice(3)}`;
+          }
+
+          if (formattedValue.length > 9) 
+          {
+            formattedValue = `${formattedValue.slice(0, 9)}-${formattedValue.slice(9)}`;
+          }
+
+        return formattedValue;
+
+        };
     // add each number to the phone number input bar
     numbers.forEach(number => {
       number.addEventListener('click', () => {
         const selectedNumber = number.getAttribute('data-number');
-        phoneNumberInput.value += selectedNumber;
-
+        let currentPN = '';
+        if (currentPN.length < 10)
+        {
+        currentPN += selectedNumber;
+        phoneNumberInput.value += phoneNumberFormat(currentPN);
+        }
 
         if(phoneNumberInput.value.length === 10)
         {
-        const submitPhoneNumber = confirm(`Is this your phone number: ${phoneNumberInput.value}?`);
 
+        const submitPhoneNumber = confirm(`Is this your phone number: ${phoneNumberInput.value}?`);
+        
         if(submitPhoneNumber)
         {
             alert(`Submitted phone number: ${phoneNumberInput.value}`);
