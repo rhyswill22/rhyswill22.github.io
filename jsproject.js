eab4d8774685c2f0539cf807b3050629d0b443ae
rhyswill22 <rhwi3413@colorado.edu>
2024-07-06 15:17:51 -0600

    Update jsproject.js

--- jsproject.js
+++ jsproject.js
@@ -14,7 +14,7 @@
             formattedValue = `(${formattedValue.slice(0, 3)}) ${formattedValue.slice(3)}`;
           }
 
-          if (formattedValue.length == 10) 
+          if (formattedValue.length > 9) 
           {
             formattedValue = `${formattedValue.slice(0, 9)}-${formattedValue.slice(9)}`;
           }
